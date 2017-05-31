1a3d4d1ff28e9276d79c9407303c32765e612161
Unknown <ASeg123@ya.ru>
2017-05-31 18:11:42 +0800

    task 2002 complete

--- 2.JavaCore/src/com/javarush/task/task20/task2002/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task20/task2002/Solution.java
@@ -1,6 +1,7 @@
 package com.javarush.task.task20.task2002;
 
 import java.io.*;
+import java.sql.Date;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,18 +13,25 @@ public class Solution {
         //you can find your_file_name.tmp in your TMP directory or fix outputStream/inputStream according to your real file location
         //вы можете найти your_file_name.tmp в папке TMP или исправьте outputStream/inputStream в соответствии с путем к вашему реальному файлу
         try {
-            File your_file_name = File.createTempFile("your_file_name", null);
+            File your_file_name = File.createTempFile("c:/1/1.txt", null);
             OutputStream outputStream = new FileOutputStream(your_file_name);
             InputStream inputStream = new FileInputStream(your_file_name);
 
             JavaRush javaRush = new JavaRush();
-            //initialize users field for the javaRush object here - инициализируйте поле users для объекта javaRush тут
+            User user = new User();
+            user.setFirstName("Lol");
+            user.setLastName("Kek");
+            user.setBirthDate(Date.valueOf("1970-01-01"));
+            user.setMale(Boolean.parseBoolean("true"));
+            user.setCountry(User.Country.valueOf("RUSSIA"));
+            javaRush.users.add(user);
             javaRush.save(outputStream);
             outputStream.flush();
 
             JavaRush loadedObject = new JavaRush();
             loadedObject.load(inputStream);
-            //check here that javaRush object equals to loadedObject object - проверьте тут, что javaRush и loadedObject равны
+            if (javaRush.equals(loadedObject))
+                System.out.println("assdyh");
 
             outputStream.close();
             inputStream.close();
@@ -41,11 +49,33 @@ public class Solution {
         public List<User> users = new ArrayList<>();
 
         public void save(OutputStream outputStream) throws Exception {
-            //implement this method - реализуйте этот метод
+            PrintWriter writer = new PrintWriter(outputStream);
+            String isUser = users.isEmpty() ? "no" : "yes";
+            writer.println(isUser);
+            if (!isUser.isEmpty()){
+                for (int i = 0; i < users.size(); i++) {
+                    writer.println(users.get(i).getFirstName()+" "+users.get(i).getLastName()+" " +users.get(i).getBirthDate().getTime()+" "+users.get(i).isMale()+" "+users.get(i).getCountry());
+                }
+            }
+            writer.flush();
         }
 
         public void load(InputStream inputStream) throws Exception {
-            //implement this method - реализуйте этот метод
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+            String isUser = reader.readLine();
+            if (isUser.equals("yes")){
+                while (reader.ready()){
+                    String[] strings = reader.readLine().split(" ");
+                    User user = new User();
+                    user.setFirstName(strings[0]);
+                    user.setLastName(strings[1]);
+                    user.setBirthDate(new Date(Long.parseLong(strings[2])));
+                    user.setMale(Boolean.parseBoolean(strings[3]));
+                    user.setCountry(User.Country.valueOf(strings[4]));
+                    users.add(user);
+                }
+            }
+            reader.close();
         }
 
         @Override
