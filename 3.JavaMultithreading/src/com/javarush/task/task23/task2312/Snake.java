414633112bfc7acce823d500c000a47d61dd7d84
Averonchik <ASeg123@ya.ru>
2017-06-10 00:04:12 +0800

    task 2312 16/18 complete

---
+++ 3.JavaMultithreading/src/com/javarush/task/task23/task2312/Snake.java
@@ -0,0 +1,66 @@
+package com.javarush.task.task23.task2312;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Averon on 6/9/2017.
+ */
+public class Snake {
+    private List<SnakeSection> sections;
+    private boolean isAlive;
+    private SnakeDirection direction;
+
+    public List<SnakeSection> getSections() {
+        return sections;
+    }
+
+    public boolean isAlive() {
+        return isAlive;
+    }
+
+    public SnakeDirection getDirection() {
+        return direction;
+    }
+
+    public void setDirection(SnakeDirection direction) {
+        this.direction = direction;
+    }
+
+    public Snake(int x, int y) {
+        sections = new ArrayList<>();
+        sections.add(new SnakeSection(x, y));
+        isAlive = true;
+    }
+    public int getX(){
+        return sections.get(0).getX();
+    }
+    public int getY(){
+        return sections.get(0).getY();
+    }
+    public void move(){
+        if (!isAlive)
+            return;
+        else if (direction==SnakeDirection.DOWN)
+            move(0, 1);
+        else if (direction==SnakeDirection.UP)
+            move(0, -1);
+        else if (direction==SnakeDirection.LEFT)
+            move(-1,0);
+        else if (direction==SnakeDirection.RIGHT)
+            move(1, 0);
+    }
+    public void move(int x, int y){}
+    public void checkBorders(SnakeSection head) {
+        if (isAlive==true) {
+            if (head.getY() >= Room.game.getHeight() || head.getX() >= Room.game.getWidth() || head.getX() < 0 || head.getY() < 0)
+                isAlive = false;
+        }
+    }
+    public void checkBody(SnakeSection head) {
+        if (isAlive==true) {
+            if (sections.contains(head))
+                isAlive = false;
+        }
+    }
+}
