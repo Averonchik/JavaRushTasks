992120d7c41e88afbf75616d211ea012ac2edb2d
Averonchik <ASeg123@ya.ru>
2017-06-25 20:17:56 +0800

    task 2409 complete

---
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2409/Item.java
@@ -0,0 +1,10 @@
+package com.javarush.task.task24.task2409;
+
+/**
+ * Created by Averon on 6/25/2017.
+ */
+public interface Item {
+    public int getId();
+    public double getPrice();
+    public String getTM();
+}
---
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2409/Jeans.java
@@ -0,0 +1,9 @@
+package com.javarush.task.task24.task2409;
+
+/**
+ * Created by Averon on 6/25/2017.
+ */
+public interface Jeans extends Item {
+    public int getLength();
+    public int getSize();
+}
